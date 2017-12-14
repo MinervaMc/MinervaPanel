d0a14f443c396598efceeddd7506796a3ad357be
Martin Zeller <mz.bremerhaven@gmail.com>
2017-12-14 02:52:34 +0100

    Add middleware for reverse proxy

--- panel.py
+++ panel.py
@@ -271,7 +271,30 @@ def init_db(db: sqlite3.Connection) -> None:
     c.execute("CREATE TABLE IF NOT EXISTS user (username TEXT, password TEXT, salt TEXT)")
 
 
+class ReverseProxyMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get("HTTP_X_SCRIPT_NAME", "")
+        if script_name:
+            environ["SCRIPT_NAME"] = script_name
+            path_info = environ["PATH_INFO"]
+            if path_info.startswith(script_name):
+                environ["PATH_INFO"] = path_info[len(script_name):]
+
+        scheme = environ.get("HTTP_X_SCHEME", "")
+        if scheme:
+            environ["wsgi.url_scheme"] = scheme
+
+        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
+        if server:
+            environ['HTTP_HOST'] = server
+        return self.app(environ, start_response)
+
+
 app.secret_key = secrets.secret_key
 
 if __name__ == "__main__":
+    app.wsgi_app = ReverseProxyMiddleware(app.wsgi_app)
     app.run(port=15000)
